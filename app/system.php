3f8985598e0136b4eea9b146d816d340a121506a
Christopher <github@dragonfire1119.com>
2012-04-09 18:29:00 -0500

    Add in app/system.php

---
+++ app/system.php
@@ -0,0 +1,47 @@
+<?php
+/**************************************************************************
+ * Dragonfire1119 Membership System AKA DMS
+ * Copyright (C) 2012  Christopher Hicks
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ **************************************************************************/
+
+/**
+ * This is the main system file
+ */
+class System {
+
+	function __construct() {
+
+		define("DIR_MODULES", "modules");
+
+		$this -> load("config.php");
+
+		$this -> load("libs/PFBC/form.php");
+		$this -> load("libs/redbeanphp/rb.php");
+		$this -> load("libs/securimage/securimage.php");
+	}
+
+	public function load($load, $deactivate=NULL) {
+		if ($deactivate != FALSE) {
+			include_once("{$load}");
+		} else {
+			include_once("{$load}");
+		}
+		
+	}
+
+}
+
+$system = new System;
