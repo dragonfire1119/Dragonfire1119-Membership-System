7f83283ce02cbdbe2fb730cd5371e5286037c095
Christopher <github@dragonfire1119.com>
2012-04-09 18:24:47 -0500

    Update the license info on the top of every .php

--- app/modules/dms/content/design/default/register/registerSuccess.php
+++ app/modules/dms/content/design/default/register/registerSuccess.php
@@ -1,3 +1,23 @@
+<?php
+/**************************************************************************
+ * Dragonfire1119 Membership System AKA DMS
+ * Copyright (C) 2012  Christopher Hicks
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ **************************************************************************/
+?>
+
 <h1>Registered!</h1>
 <p>Thank you <b> <?php echo $_SESSION['reguname']; ?> 
 	</b>, your information has been added to the database, There is one more step, check your email to very your account</p><a href=\"index.php\"> Go back to Login</a>
--- app/modules/dms/includes/core/security.php
+++ app/modules/dms/includes/core/security.php
@@ -1,24 +1,21 @@
 <?php
-
-/* Dragonfire1119 Membership Script Beta 0.2
- * Copyright (c) 2011 Christopher Hicks
- * Licensed under the GNU General Public License version 3.0 (GPLv3)
+/**************************************************************************
+ * Dragonfire1119 Membership System AKA DMS
+ * Copyright (C) 2012  Christopher Hicks
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
-
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Date: December 13, 2011
- * ------------------------------------------------------------------------------------------------ */
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ **************************************************************************/
 
 if (!defined('DMS_SECURITY')) {
 	die('Hacking attempt...');
