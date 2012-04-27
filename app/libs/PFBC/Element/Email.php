a42951957d231322aff02c4a44d7fbf102ba7861
Christopher <github@dragonfire1119.com>
2012-04-26 22:40:06 -0500

    delete

--- app/libs/PFBC/Element/Email.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Element_Email extends Element_Textbox {
-	public function render() {
-		$this->validation[] = new Validation_Email;
-		parent::render();
-	}
-}
--- app/libs/PFBC/Element/File.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Element_File extends Element {
-	protected $attributes = array("type" => "file");
-}
